a395c500e691e9f526b5998599d04f53eb51f9cd
Beltran <beltran_xv@hotmail.com>
2018-03-28 23:11:04 +0200

    hecho hasta unir con jquery

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,36 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
 }
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.queueControl.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.canEnqueue= function() {
+  if (this.queueControl.length < this.MAX_SIZE) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.enqueue = function(a) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(a);
+    return this.queueControl;
+  } else {
+    return "Queue Overflow";
+  }
+};
+
+QueueDataStructure.prototype.dequeue = function(a) {
+  if (!this.isEmpty()) {
+    return this.queueControl.pop(a)
+  } else {
+    return "Queue Underflow";
+  }
+};
